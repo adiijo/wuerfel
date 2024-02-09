9b9abf0e9e4a07f2d8b05235737b318eed6579e8
adiijo <147428794+adiijo@users.noreply.github.com>
2024-02-09 12:04:03 +0100

    Aktualisiere pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,3 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    led.unplot(pointx, pointy)
-    pointx += -1
-    led.plot(pointx, pointy)
-})
 let pointy = 0
 let pointx = 0
 pointx = randint(0, 4)
@@ -11,3 +6,26 @@ let buttonx = randint(0, 4)
 let buttony = randint(0, 2)
 led.plot(pointx, pointy)
 led.plot(buttonx, buttony)
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    led.unplot(pointx, pointy)
+    pointx += -1
+    led.plot(pointx, pointy)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    led.unplot(pointx, pointy)
+    pointx += 1
+    led.plot(pointx, pointy)
+})
+ 
+
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function() {
+  led.unplot(pointx,pointy)
+  pointy += 1 
+  led.plot(pointx,pointy) 
+})
+
+input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
+  led.unplot(pointx, pointy)
+  pointy += -1
+  led.plot(pointx, pointy)  
+})
