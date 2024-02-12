4a1aa530a88ce38949fa56b383351924fd9a2153
adiijo <147428794+adiijo@users.noreply.github.com>
2024-02-12 18:36:59 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,34 +2,30 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     led.unplot(pointx, pointy)
     pointx += -1
     led.plot(pointx, pointy)
+    if (pointx < 0) {
+        led.unplot(pointx, pointy)
+        pointx = 0
+        led.plot(pointx, pointy)
+    }
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     led.unplot(pointx, pointy)
     pointx += 1
     led.plot(pointx, pointy)
-})
-input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
-    led.unplot(pointx, pointy)
-    pointy += 1
-    led.plot(pointx, pointy)
-})
-input.onPinTouchEvent(TouchPin.P3, input.buttonEventDown(), function () {
-    led.unplot(pointx, pointy)
-    pointy += -1
-    led.plot(pointx, pointy)
-    if (pointy>=) {
-
-    } else {
-
+    if (pointx > 4) {
+        led.unplot(pointx, pointy)
+        pointx = 4
+        led.plot(pointx, pointy)
     }
 })
-
+let buttony = 0
+let buttonx = 0
 let pointy = 0
 let pointx = 0
 pointx = randint(0, 4)
-pointy = randint(2, 4)
-let buttonx = randint(0, 4)
-let buttony = randint(0, 2)
+pointy = 4
+buttonx = randint(0, 4)
+buttony = 0
 led.plot(pointx, pointy)
 led.plot(buttonx, buttony)
 
